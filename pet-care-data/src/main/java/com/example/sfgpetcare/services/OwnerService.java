7c703bd718049a1bfa22a3c07c65e7f71f603a4a
rajeshbattula631 <rajesh.battula631@gmail.com>
2021-04-18 15:57:06 +0530

    Refactored Service Interface to Common Base Interface. Closes #11.

--- pet-care-data/src/main/java/com/example/sfgpetcare/services/OwnerService.java
+++ pet-care-data/src/main/java/com/example/sfgpetcare/services/OwnerService.java
@@ -2,11 +2,7 @@ package com.example.sfgpetcare.services;
 
 import com.example.sfgpetcare.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
