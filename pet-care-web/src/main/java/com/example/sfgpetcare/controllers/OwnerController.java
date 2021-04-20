304b6a384fcbf9e7bfeb85a93ac305e2e19ec0d6
rajeshbattula631 <rajesh.battula631@gmail.com>
2021-04-20 09:51:59 +0530

    Displayed List all Owners on Owners Index Page. Closes #16.

--- pet-care-web/src/main/java/com/example/sfgpetcare/controllers/OwnerController.java
+++ pet-care-web/src/main/java/com/example/sfgpetcare/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package com.example.sfgpetcare.controllers;
 
+import com.example.sfgpetcare.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
