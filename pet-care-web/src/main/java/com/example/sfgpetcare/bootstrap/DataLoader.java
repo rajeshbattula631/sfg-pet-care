a518ce584a426763f1e9174ffab4aca1b42cae07
rajeshbattula631 <rajesh.battula631@gmail.com>
2021-04-20 10:57:35 +0530

    Updated the Map Implementation to Manage Setting of ID Property. Closes #23.

--- pet-care-web/src/main/java/com/example/sfgpetcare/bootstrap/DataLoader.java
+++ pet-care-web/src/main/java/com/example/sfgpetcare/bootstrap/DataLoader.java
@@ -25,27 +25,36 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-        owner1.setId(1L);
+
         ownerService.save(owner1);
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-        owner2.setId(2L);
+
         ownerService.save(owner2);
+        Owner owner3 = new Owner();
+        owner3.setFirstName("Rajesh");
+        owner3.setLastName("Battula");
+        ownerService.save(owner3);
         System.out.println("Loaded Owners....");
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-        vet1.setId(1L);
+
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-        vet2.setId(2L);
 
         vetService.save(vet2);
+
+        Vet vet3 = new Vet();
+        vet3.setFirstName("Rod");
+        vet3.setLastName("Porter");
+        vetService.save(vet3);
+
         System.out.println("Loaded Vets....");
     }
 }
