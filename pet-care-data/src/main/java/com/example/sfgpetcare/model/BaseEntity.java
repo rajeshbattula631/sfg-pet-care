d3162c69b25f3c0b7cb2d9de72765e75edc7972d
rajeshbattula631 <rajesh.battula631@gmail.com>
2021-04-26 18:30:40 +0530

    Implemented Base Entity and Issue rectified. Closes #35

--- pet-care-data/src/main/java/com/example/sfgpetcare/model/BaseEntity.java
+++ pet-care-data/src/main/java/com/example/sfgpetcare/model/BaseEntity.java
@@ -6,8 +6,6 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-
-
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
