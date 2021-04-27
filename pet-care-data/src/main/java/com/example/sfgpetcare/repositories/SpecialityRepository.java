404315ae2178f6cc95be711a83ab9c025ab7433b
rajeshbattula631 <rajesh.battula631@gmail.com>
2021-04-27 10:38:33 +0530

    Created Spring Data JPA Service for Speciality. Closes #44

--- pet-care-data/src/main/java/com/example/sfgpetcare/repositories/SpecialityRepository.java
+++ pet-care-data/src/main/java/com/example/sfgpetcare/repositories/SpecialityRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SepecialityRepository extends CrudRepository<Speciality, Long> {
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
 }
